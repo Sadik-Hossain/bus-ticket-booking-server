8f33a40098611839d13d0d5a817ebb602c471b99
sadik <po480373@gmai.com>
2023-03-18 22:19:17 +0600

    first commit

---
+++ configs/dbconn.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const MONGO = process.env.MONGO_PROD || process.env.MONGO_DEV;
+mongoose.set("strictQuery", false);
+const dbconn = async () => {
+  try {
+    const {
+      connection: { host, port, name },
+    } = await mongoose.connect(MONGO);
+
+    console.log("mongo started on", host, port, name);
+  } catch (error) {
+    console.log(error.message);
+    // throw error;
+  }
+};
+module.exports = dbconn;
